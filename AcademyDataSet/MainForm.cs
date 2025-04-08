b53fa9e82908a82310cf5d8e5e82eaa18b36753e
ASUTP <ASUTP@DESKTOP-SSAKK7M>
2025-04-08 14:44:19 +0300

    Optomization stage 1 DONE

--- AcademyDataSet/MainForm.cs
+++ AcademyDataSet/MainForm.cs
@@ -19,21 +19,55 @@ namespace AcademyDataSet
         readonly string CONNECTION_STRING = "";
         SqlConnection connection;
         DataSet GroupsRelatedData;
+        List<string> tables;
         public MainForm()
         {
             InitializeComponent();
             AllocConsole();
             CONNECTION_STRING = ConfigurationManager.ConnectionStrings["PV_319_Import"].ConnectionString;
             connection = new SqlConnection(CONNECTION_STRING);
-            Console.WriteLine( CONNECTION_STRING);
-            LoadGroupsRelatedData();
+            Console.WriteLine(CONNECTION_STRING);
+            tables = new List<string>();
+            GroupsRelatedData = new DataSet(nameof(GroupsRelatedData));
+            // LoadGroupsRelatedData();
+            Check();
+        }
+        public void AddTable(string table, string columns)
+        {
+            string[] separated_colums = columns.Split(',');
+            GroupsRelatedData.Tables.Add(table);
+            for (int i = 0; i < separated_colums.Length; i++)
+            {
+                GroupsRelatedData.Tables[table].Columns.Add(separated_colums[i]);
+                GroupsRelatedData.Tables[table].PrimaryKey =
+                new DataColumn[] { GroupsRelatedData.Tables[table].Columns[separated_colums[0]] };
+                tables.Add($"{table},{columns}");
+            }
+        }
+        public void AddRelation(string name, string child, string parent)
+        {
+            GroupsRelatedData.Relations.Add
+                (
+                    name,
+                    GroupsRelatedData.Tables[parent.Split(',')[0]].Columns[parent.Split(',')[1]],
+                    GroupsRelatedData.Tables[child.Split(',')[0]].Columns[child.Split(',')[1]]
+
+                );
+        }
+        public new void Load()
+        {
+            string[] tables = this.tables.ToArray();
+            for (int i = 0; i < tables.Length; i++)
+            {
+                string cmd = $"SELECT * FROM {tables[i].Split(',')[0]}";
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd, connection);
+                adapter.Fill(GroupsRelatedData.Tables[tables[i].Split(',')[0]]);
+            }
         }
         void LoadGroupsRelatedData()
         {
             Console.WriteLine(nameof(GroupsRelatedData));
             //1) Создаем "DataSet"
-
-            GroupsRelatedData = new DataSet(nameof(GroupsRelatedData));
 
             //2) Добавляем таблицы в DataSet
 
@@ -41,14 +75,14 @@ namespace AcademyDataSet
             const string dst_col__directions_id = "direction_id";
             const string dst_col__directions_name = "direction_name";
             GroupsRelatedData.Tables.Add(dsTable_Directions);
-            GroupsRelatedData.Tables[dsTable_Directions].Columns.Add(dst_col__directions_id,typeof(byte));
+            GroupsRelatedData.Tables[dsTable_Directions].Columns.Add(dst_col__directions_id, typeof(byte));
             GroupsRelatedData.Tables[dsTable_Directions].Columns.Add(dst_col__directions_name, typeof(string));
             GroupsRelatedData.Tables[dsTable_Directions].PrimaryKey =
                 new DataColumn[] { GroupsRelatedData.Tables[dsTable_Directions].Columns[dsTable_Directions] };
             const string dsTable_Groups = "Groups";
             const string dst_Groups_col_group_id = "group_id";
             const string dst_Gtoups_col_group_name = "group_name";
-            const string dst_Groups_col_direction ="direction";
+            const string dst_Groups_col_direction = "direction";
             GroupsRelatedData.Tables.Add(dsTable_Groups);
             GroupsRelatedData.Tables[dsTable_Groups].Columns.Add(dst_Groups_col_group_id, typeof(int));
             GroupsRelatedData.Tables[dsTable_Groups].Columns.Add(dst_Gtoups_col_group_name, typeof(string));
@@ -71,12 +105,34 @@ namespace AcademyDataSet
             directionsAdapter.Fill(GroupsRelatedData.Tables[dsTable_Directions]);
             groupsAdapter.Fill(GroupsRelatedData.Tables[dsTable_Groups]);
             connection.Close();
-            foreach (DataRow row in GroupsRelatedData.Tables[dsTable_Directions].Rows) 
-            Console.WriteLine($"{row[dst_col__directions_id]}\t{row[dst_col__directions_name]}");
+            foreach (DataRow row in GroupsRelatedData.Tables[dsTable_Directions].Rows)
+                Console.WriteLine($"{row[dst_col__directions_id]}\t{row[dst_col__directions_name]}");
             Console.WriteLine("=============================================");
-            foreach(DataRow row in GroupsRelatedData.Tables[dsTable_Groups].Rows)
+            foreach (DataRow row in GroupsRelatedData.Tables[dsTable_Groups].Rows)
                 Console.WriteLine($"{row[dst_Groups_col_group_id]}\t" +
                     $"{row[dst_Gtoups_col_group_name]}\t{row.GetParentRow(dsRelation_groupsDirections)[dst_col__directions_name]}");
+        }
+        void Print(string table)
+        {
+            Console.WriteLine("\n---------------------------------\n");
+            foreach(DataRow row in GroupsRelatedData.Tables[table].Rows )
+            {
+                for(int i = 0; i<row.ItemArray.Length;i++)
+                {
+                    Console.Write(row[i].ToString()+"\t"); ;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n---------------------------------\n");
+        }
+        void Check()
+        {
+            AddTable("Directions", "direction_id,direction_name");
+            AddTable("Groups", "group_id,group_name,direction");
+            AddRelation("GroupsDirections", "Groups,direction", "Directions,direction_id");
+            Load();
+            Print("Directions");
+            Print("Groups");
         }
         [DllImport("kernel32.dll")]
         public static extern bool AllocConsole();
